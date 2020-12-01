bdd4b5f853d218e9468e60bbcbd0c2aec99a8f2d
punker76 <punkerat76@gmail.com>
2020-12-01 23:02:03 +0100

    --- Day 1: Report Repair --- play with VC code compile and run

---
+++ 01/out/sumto2020.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
+let data = fs.readFileSync('input.txt', 'utf8')
+    .trim()
+    .split('\n')
+    .map(l => +l);
+console.log(`Data has been read ${data.length}`);
+data.forEach((first, idx) => {
+    let second = data.filter((v, i) => i > idx).find(v => first + v === 2020);
+    if (second) {
+        console.log(`${first} + ${second} is 2020, so the product is ${first * second}`);
+        return;
+    }
+});
+data.forEach((first, idx1) => {
+    data.filter((v, i) => i > idx1).forEach((second, idx2) => {
+        let third = data.filter((v, i) => i > idx2).find(v => first + second + v === 2020);
+        if (third) {
+            console.log(`${first} + ${second} + ${third} is 2020, so the product is ${first * second * third}`);
+            return;
+        }
+    });
+});
+//# sourceMappingURL=sumto2020.js.map
