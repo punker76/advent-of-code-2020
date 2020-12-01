50cdc3ffe44e6335529baaf901974f7503708b49
punker76 <punkerat76@gmail.com>
2020-12-01 23:03:03 +0100

    --- Day 1: Report Repair --- minor code change

--- 01/sumto2020.ts
+++ 01/sumto2020.ts
@@ -1,31 +1,27 @@
-const readline = require('readline');
-const fs = require('fs');
+import fs = require('fs');
 
-let data: number[] = [];
+let data: number[] =
+    fs.readFileSync('input.txt', 'utf8')
+        .trim()
+        .split('\n')
+        .map(l => +l);
 
-const reader = readline.createInterface({
-    input: fs.createReadStream('./input.txt'),
-    console: false
+console.log(`Data has been read ${data.length}`);
+
+data.forEach((first, idx) => {
+    let second = data.filter((v, i) => i > idx).find(v => first + v === 2020);
+    if (second) {
+        console.log(`${first} + ${second} is 2020, so the product is ${first * second}`);
+        return;
+    }
 });
 
-reader.on('line', (line: any) => {
-    data.push(+line);
-});
-
-reader.on("close", () => {
-    console.log(`Data has been read ${data.length}`);
-
-    data.forEach((first, idx1) => {
-        data.filter((v, i) => i > idx1).forEach((second, idx2) => {
-            if (first + second === 2020) {
-                console.log(`${first} + ${second} is 2020, so the product is ${first * second}`);
-            }
-
-            let third = data.filter((v, i) => i > idx2).find(v => first + second + v === 2020);
-            if (third) {
-                console.log(`${first} + ${second} + ${third} is 2020, so the product is ${first * second * third}`);
-            }
-        });
+data.forEach((first, idx1) => {
+    data.filter((v, i) => i > idx1).forEach((second, idx2) => {
+        let third = data.filter((v, i) => i > idx2).find(v => first + second + v === 2020);
+        if (third) {
+            console.log(`${first} + ${second} + ${third} is 2020, so the product is ${first * second * third}`);
+            return;
+        }
     });
-
 });
