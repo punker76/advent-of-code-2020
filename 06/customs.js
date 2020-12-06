55bcf0a5d70445cf0767e327a0b64a26caf8bc52
punker76 <punkerat76@gmail.com>
2020-12-06 20:03:55 +0100

    --- Day 6: Custom Customs --- Part two

--- 06/customs.js
+++ 06/customs.js
@@ -7,15 +7,31 @@ const data =
         .replace(/\r\n/g, '\n')
         .split('\n');
 
-const groups = [''];
+const groups = [[]];
 
 data.forEach(element => {
     if (element === '') {
-        groups.push('');
+        groups.push([]);
     }
-    groups[groups.length - 1] += element;
+    else {
+        groups[groups.length - 1].push(element);
+    }
 });
 
-var count = groups.map(v => new Set([...v]).size).reduce((v1, v2) => v1 + v2);
+var count = groups
+    .map(v => new Set(v.join('')).size)
+    .reduce((v1, v2) => v1 + v2);
 
-console.log(`Sum of group count is ${count}`);
+console.log(`Part one: Sum of group count is ${count}`);
+
+var count2 = groups
+    .map(v => {
+        let p = [...v[0]];
+        for (let i = 1; i < v.length; i++) {
+            p = p.filter(value => [...v[i]].includes(value));
+        }
+        return new Set(p).size;
+    })
+    .reduce((v1, v2) => v1 + v2);
+
+console.log(`Part two: Sum of group count is ${count2}`);
