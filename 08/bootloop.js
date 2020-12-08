c20d397bcbcfb1e09a25d6a4c3b43892b8d109ca
punker76 <punkerat76@gmail.com>
2020-12-08 13:36:18 +0100

    --- Day 8: Handheld Halting --- Part two

--- 08/bootloop.js
+++ 08/bootloop.js
@@ -6,29 +6,90 @@ const data =
         .replace(/\r\n/g, '\n')
         .split('\n');
 
-const usedInstructons = new Set();
-let accumulator = 0;
-let line = 0;
+/**
+ * @param {string[]} [theData]
+ */
+function isInfinite(theData) {
+    const usedInstructons = new Set();
+    let accumulator = 0;
+    let line = 0;
 
-while (true) {
-    if (usedInstructons.has(line)) {
-        break;
+    while (line < theData.length) {
+        if (usedInstructons.has(line)) {
+            break;
+        }
+
+        usedInstructons.add(line);
+        const [arg, op] = theData[line].split(' ');
+        switch (arg) {
+            case 'acc':
+                accumulator += +op;
+                line++;
+                break;
+            case 'jmp':
+                line += +op;
+                break;
+            default:
+                line++;
+                break;
+        }
     }
 
-    usedInstructons.add(line);
-    const [arg, op] = data[line].split(' ');
-    switch (arg) {
-        case 'acc':
-            accumulator += +op;
-            line++;
-            break;
-        case 'jmp':
-            line += +op;
-            break;
-        default:
-            line++;
-            break;
-    }
+    return accumulator;
 }
 
-console.log(`The value of the accumulator is ${accumulator}`);
+
+console.log(`The value of the accumulator is ${isInfinite(data)}`);
+
+/**
+ * @param {string[]} [theData]
+ */
+function getAccumulator(theData) {
+    const usedInstructons = new Set();
+    let accumulator = 0;
+    let line = 0;
+
+    while (line < theData.length) {
+        if (usedInstructons.has(line)) {
+            return -1;
+        }
+
+        usedInstructons.add(line);
+        const [arg, op] = theData[line].split(' ');
+        switch (arg) {
+            case 'acc':
+                accumulator += +op;
+                line++;
+                break;
+            case 'jmp':
+                line += +op;
+                break;
+            default:
+                line++;
+                break;
+        }
+    }
+
+    return accumulator;
+}
+
+for (let index = 0; index < data.length; index++) {
+    const [arg, op] = data[index].split(' ');
+
+    if (arg === 'jmp' && +op != null) {
+        const newData = [...data];
+        newData[index] = newData[index].replace('jmp', 'nop');
+        const count = getAccumulator(newData);
+        if (count >= 0) {
+            console.log(`The value of the accumulator is ${count}`);
+        }
+    }
+    else if (arg === 'nop' && +op != 0) {
+        const newData = [...data];
+        newData[index] = newData[index].replace('nop', 'jmp');
+        const count = getAccumulator(newData);
+        if (count >= 0) {
+            console.log(`The value of the accumulator is ${count}`);
+        }
+    }
+}
