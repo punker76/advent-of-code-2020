89a3fa0d53262606dba66e8187bafc4c1e6e7870
punker76 <punkerat76@gmail.com>
2020-12-09 12:23:36 +0100

    --- Day 9: Encoding Error --- Part two

--- 09/xmas.js
+++ 09/xmas.js
@@ -15,15 +15,24 @@ const preamble = 25;
  */
 function isSumOf2(n, range) {
     return range.some((value, i) => range.slice(i + 1).some(v => value + v === n));
+}
 
-    // for (let index = 0; index < range.length; index++) {
-    //     const exists = range.slice(index + 1).some(v => range[index] + v === n);
-    //     if (exists) {
-    //         return true;
-    //     }
-    // }
+/**
+ * @param {number} n
+ * @param {number[]} range
+ */
+function getWeakness(n, range) {
+    for (let idx1 = 0; idx1 < range.length; idx1++) {
+        let result = [range[idx1]];
+        for (let idx2 = idx1 + 1; idx2 < range.length; idx2++) {
+            result.push(range[idx2]);
+            if (result.reduce((v1, v2) => v1 + v2) === n) {
+                return Math.min(...result) + Math.max(...result);
+            }
+        }
+    }
 
-    // return false;
+    return 0;
 }
 
 for (let index = preamble; index < data.length; index++) {
@@ -31,6 +40,7 @@ for (let index = preamble; index < data.length; index++) {
     const isValid = isSumOf2(n, data.slice(index - preamble, index));
     if (!isValid) {
         console.log(`The first invalid number is ${n}`);
+        console.log(`The encryption weakness is ${getWeakness(n, data)}`);
         break;
     }
 }
