e9b9b3662dd22471c25b1aed7f1924c392f100f7
punker76 <punkerat76@gmail.com>
2020-12-15 13:19:17 +0100

    --- Day 15: Rambunctious Recitation --- Part two

--- 15/game.ts
+++ 15/game.ts
@@ -1,31 +1,45 @@
-const partOne = (input: number[], count: number) => {
+const solvePuzzle = (input: number[], count: number) => {
     const startInput = [...input];
+    const map = new Map<number, number>();
+    input.slice(0, input.length - 1).forEach(n => {
+        map.set(n, input.indexOf(n));
+    });
 
     for (let turn = input.length; turn < count; turn++) {
         let nextNumber = input[turn - 1];
 
-        const firstSpoken = input.lastIndexOf(nextNumber);
-        if (firstSpoken >= 0) {
-            const secondSpoken = input.slice(0, firstSpoken).lastIndexOf(nextNumber);
-            if (secondSpoken >= 0) {
-                nextNumber = firstSpoken - secondSpoken;
-            }
-            else {
-                nextNumber = 0;
-            }
+        const secondSpoken: number = map.get(nextNumber);
+        if (secondSpoken !== undefined) {
+            map.set(nextNumber, turn - 1);
+            nextNumber = turn - 1 - secondSpoken;
+        }
+        else {
+            map.set(nextNumber, turn - 1);
+            nextNumber = 0;
         }
 
         input.push(nextNumber);
     }
 
-    console.log(`Part One: Given the starting numbers ${startInput}, the ${count}th number spoken is ${input[input.length - 1]}`);
+    console.log(`Given the starting numbers ${startInput}, the ${count}th number spoken is ${input[input.length - 1]}`);
 }
 
-partOne([0, 3, 6], 2020);
-partOne([1, 3, 2], 2020);
-partOne([2, 1, 3], 2020);
-partOne([1, 2, 3], 2020);
-partOne([2, 3, 1], 2020);
-partOne([3, 2, 1], 2020);
-partOne([3, 1, 2], 2020);
-partOne([2, 0, 1, 9, 5, 19], 2020);
+console.log('>>> Part One <<<');
+solvePuzzle([0, 3, 6], 2020);
+solvePuzzle([1, 3, 2], 2020);
+solvePuzzle([2, 1, 3], 2020);
+solvePuzzle([1, 2, 3], 2020);
+solvePuzzle([2, 3, 1], 2020);
+solvePuzzle([3, 2, 1], 2020);
+solvePuzzle([3, 1, 2], 2020);
+solvePuzzle([2, 0, 1, 9, 5, 19], 2020);
+
+console.log('>>> Part Two <<<');
+solvePuzzle([0, 3, 6], 30000000);
+solvePuzzle([1, 3, 2], 30000000);
+solvePuzzle([2, 1, 3], 30000000);
+solvePuzzle([1, 2, 3], 30000000);
+solvePuzzle([2, 3, 1], 30000000);
+solvePuzzle([3, 2, 1], 30000000);
+solvePuzzle([3, 1, 2], 30000000);
+solvePuzzle([2, 0, 1, 9, 5, 19], 30000000);
