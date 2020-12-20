d258e37a3630c669319654c35a4b4352dd1127ae
punker76 <punkerat76@gmail.com>
2020-12-20 17:37:33 +0100

    --- Day 19: Monster Messages --- Part two

--- 19/message.ts
+++ 19/message.ts
@@ -43,4 +43,33 @@ const partOne = (rules: string[], messages: string[]) => {
     console.log(`Part One: ${count} messages completely match rule 0`);
 }
 
+const partTwo = (rules: string[], messages: string[]) => {
+    const solvedRules = {};
+    const rulesMap = {};
+    for (const rule of rules) {
+        const tmp = rule.split(':').map(x => x.trim());
+        rulesMap[tmp[0]] = tmp[1];
+    }
+    // rulesMap['8'] = '42 | 42 8';
+    // rulesMap['11'] = '42 32 | 42 11 31';
+    const firstRule = rulesMap[0];
+    solveValue(firstRule, rulesMap, solvedRules);
+    const rule42 = solveValue(rulesMap['42'], rulesMap, solvedRules);
+    const rule31 = solveValue(rulesMap['31'], rulesMap, solvedRules);
+    const regex = new RegExp(`^(?<g42>${rule42}+)(?<g31>${rule31}+)$`);
+    const regexRule42 = new RegExp(rule42, 'g');
+    const regexRule31 = new RegExp(rule31, 'g');
+    const count = messages.filter(m => {
+        const matches = regex.exec(m);
+        if (matches) {
+            const m42 = matches.groups.g42.match(regexRule42).length;
+            const m31 = matches.groups.g31.match(regexRule31).length;
+            return m42 > m31;
+        }
+        return false;
+    }).length;
+    console.log(`Part Two: ${count} messages completely match rule 0`);
+}
+
 partOne(data[0].replace(/"/g, '').split('\n'), data[1].split('\n'));
+partTwo(data[0].replace(/"/g, '').split('\n'), data[1].split('\n'));
